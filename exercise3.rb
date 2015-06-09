5a6eba2f389416b8e08f99defcac381f0905ace1
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-06-09 14:36:40 -0400

    exercise 3 final with age question and answer

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,7 @@ puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
 
+puts "How old are you?"
+age = gets.chomp 
+year_born = 2015 - age.to_i
+puts "Therefore you were born in #{year_born}" 
