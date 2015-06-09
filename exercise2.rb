cb02b049886d04321484578a1f6b5438a9aabe44
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-06-09 13:59:45 -0400

    new exercise2 file

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+# 1.- Calculate tip for a 55 dollar meal
+tip = 55 * 0.20
+puts "A good tip for a $55 meal is $#{tip}"
+
+# 2.- Adding a string and integer changing data types
+number = 5
+string = " is my favorite number!!"
+puts number.to_s + string
+
+#3.- String Interpolation to get the result of 45628 times 7839
+a = 45628
+b = 7839
+puts "The result of multiplying 45628 * 7839 is #{a * b}."
+
+#4.- Value of expression
+puts (true && true) || (false && false) || !(false && false)
